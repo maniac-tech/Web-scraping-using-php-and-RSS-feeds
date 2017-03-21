61a69f751344a4e0d3d9f45bbb0923c9b689c909
innovater21 <ajha025@gmail.com>
2017-03-21 16:01:07 +0530

    updated the routing

--- testmain.py
+++ testmain.py
@@ -4,16 +4,51 @@ from flask import redirect
 
 import feedparser
 import pprint 
+dlm = [] 
+slm = []
 
-d=feedparser.parse("https://news.google.com/?output=rss")
+fp=open("parsedfeed.py","w")
+d=feedparser.parse("http://timesofindia.indiatimes.com/rssfeedstopstories.cms")
 # print(d)
 #prettu print
 pp=pprint.pformat(d,indent=4)
 
 #writing the feed to file 
-#fp.write(pp)
-print(pp)
+fp.write(pp)
+#print(pp)
+list = d['entries']
+i=0
+#loop for taking out all the links
+m=len(list)
+while(i<m):
+	dict_of_list = list[i]
 
+	#printing the list recovered from entries:
+	# print(list)
+
+	#formatting the list generated:
+	rr=pprint.pformat(list,indent=4)
+
+	#storing the formatted list into file:
+	
+
+	#printing the formated list
+	# print(rr)
+
+	#formatting dict_of_list:
+	dl = pprint.pformat(dict_of_list['links'],indent=4)
+	dlm.append(dl)
+	sl = pprint.pformat(dict_of_list['summary'],indent=4)
+	slm.append(sl)
+	#printing the formatted links value inside dict_of_list:
+	print("Article No {}".format(i))
+	# print(i)
+	print(dl)
+	print(sl)
+	i=i+1
+	pass
+print('printing dlm')
+print(dlm)
 app = Flask(__name__)
 
 #Basic route
@@ -23,7 +58,8 @@ app = Flask(__name__)
 
 
 def index():
-	return render_template('hmain.html')
+	
+	return render_template('hmain.html', SLM = slm)
 
 #http://localhost:5000/yourname
 # @app.route('/<name>')
